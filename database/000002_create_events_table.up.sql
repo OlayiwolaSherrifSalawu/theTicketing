996184605f528c469dbdced3a892fc3b25c8c159
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-05-25 17:31:12 +0100

    added and used the gen uuid

--- database/000002_create_events_table.up.sql
+++ database/000002_create_events_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS events(
-    id uuid PRIMARY KEY NOT NULL,
+    id uuid PRIMARY KEY gen_random_uuid(),
     location VARCHAR(255) NOT NULL,
-    start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    start_time TIMESTAMP WITH TIME ZONE,
     available_tickets INT NOT NULL,
     tickets_types VARCHAR(255) NOT NULL,
     event_name VARCHAR(255) NOT NULL,
