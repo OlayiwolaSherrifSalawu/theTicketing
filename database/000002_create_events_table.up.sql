7a6bf53aab49fe2cfd31744fb3b6f39e0f829056
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-05-25 17:22:13 +0100

    added migration table

--- database/000002_create_events_table.up.sql
+++ database/000002_create_events_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS events(
-    id uuid PRIMARY KEY,
-    location VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    availableTicketss INT,
-    ticketsTypes VARCHAR(255),
-    eventName VARCHAR(255),
-    totalCapacity INT
-)
+    id uuid PRIMARY KEY NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    available_tickets INT NOT NULL,
+    tickets_types VARCHAR(255) NOT NULL,
+    event_name VARCHAR(255) NOT NULL,
+    total_capacity INT NOT NULL
+);
