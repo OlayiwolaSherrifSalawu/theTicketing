05316c3b6c660d1dfde6ef68ee75c95ca70a886e
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-02 15:44:09 +0100

    decoded json on event struct

--- api/eventHandler.go
+++ api/eventHandler.go
@@ -1,7 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/OlayiwolaSherrifSalawu/theTicketing.git/pkg/model"
+)
 
 func (a *Application) CreateEvent(w http.ResponseWriter, r *http.Request) {
+	r.Body = http.MaxBytesReader(w, r.Body, 1234)
+	newEvent := new(model.Event)
+	if err := json.NewDecoder(r.Body).Decode(newEvent); err != nil {
+		a.serverError(w, err)
+		return
+	}
 	
 }
