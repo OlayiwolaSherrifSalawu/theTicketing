8dc756d5b9506f5282de18b15498dedf4d546c46
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-02 15:00:50 +0100

    added proper context

--- pkg/model/theticket/thTicketqueries.go
+++ pkg/model/theticket/thTicketqueries.go
@@ -6,14 +6,14 @@ import (
 	"github.com/OlayiwolaSherrifSalawu/theTicketing.git/pkg/model"
 )
 
-func (d *TheTicketModel) InsertT(ctx *context.Context, Event *model.Event) error {
-	stmt := "INSERT INTO events(id, location, start_time, available_tickets, tickets_types, events_name, total_capacity, created_at) values($1, $2, $3, $4, $5, $6, $7, $8)"
-	tx, err := d.DB.BeginTx(*ctx, nil)
+func (d *TheTicketModel) InsertT(ctx context.Context, Event *model.Event) error {
+	stmt := "INSERT INTO events(location, start_time, available_tickets, tickets_types, events_name, total_capacity) values($1, $2, $3, $4, $5, $6) RETURNING id"
+	tx, err := d.DB.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
-	_, err = tx.ExecContext(*ctx, stmt, Event.Id, Event.Location, Event.StartTime, Event.AvailableTickets, Event.TicketsTypes, Event.EventName, Event.TotalCapacity)
+	err = tx.QueryRowContext(ctx, stmt, Event.Location, Event.StartTime, Event.AvailableTickets, Event.TicketsTypes, Event.EventName, Event.TotalCapacity).Scan(&Event.Id)
 	if err != nil {
 		return err
 	}
