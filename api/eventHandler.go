ecb231d12c381da96cd4188ddfeaa960fdb99bcb
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-02 15:54:42 +0100

    finished createEvent

--- api/eventHandler.go
+++ api/eventHandler.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/OlayiwolaSherrifSalawu/theTicketing.git/pkg/model"
 )
@@ -10,9 +12,17 @@ import (
 func (a *Application) CreateEvent(w http.ResponseWriter, r *http.Request) {
 	r.Body = http.MaxBytesReader(w, r.Body, 1234)
 	newEvent := new(model.Event)
-	if err := json.NewDecoder(r.Body).Decode(newEvent); err != nil {
+	err := json.NewDecoder(r.Body).Decode(newEvent)
+	if err != nil {
 		a.serverError(w, err)
 		return
 	}
-	
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*3)
+	defer cancel()
+	err = a.TheTicket.InsertT(ctx, newEvent)
+	if err != nil {
+		a.serverError(w, err)
+		return
+	}
+	w.WriteHeader(201)
 }
