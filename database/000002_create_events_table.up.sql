36997521825ebd728fcd9d0b67f95c4d78cbf452
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-02 11:37:24 +0100

    added and implemented event model and migration

--- database/000002_create_events_table.up.sql
+++ database/000002_create_events_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS events(
-    id uuid PRIMARY KEY gen_random_uuid(),
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     location VARCHAR(255) NOT NULL,
-    start_time TIMESTAMP WITH TIME ZONE,
+    start_time TIMESTAMP WITH TIME ZONE NOT NULL,
     available_tickets INT NOT NULL,
     tickets_types VARCHAR(255) NOT NULL,
     event_name VARCHAR(255) NOT NULL,
---
+++ pkg/model/theticket/thTicketqueries.go
@@ -0,0 +1,11 @@
+package theticket
+
+import (
+	"context"
+
+	"github.com/OlayiwolaSherrifSalawu/theTicketing.git/pkg/model"
+)
+
+func (d *TheTicketModel) InsertT(ctx *context.Context, Event *model.Event) error {
+	
+}
