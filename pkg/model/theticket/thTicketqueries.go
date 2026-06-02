52febb0f93147a37db8a122bfe55eaecba353e9c
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-06-02 11:53:27 +0100

    added insert for the thicketing

--- pkg/model/theticket/thTicketqueries.go
+++ pkg/model/theticket/thTicketqueries.go
@@ -7,5 +7,16 @@ import (
 )
 
 func (d *TheTicketModel) InsertT(ctx *context.Context, Event *model.Event) error {
-	
+	stmt := "INSERT INTO events(id, location, start_time, available_tickets, tickets_types, events_name, total_capacity, created_at) values($1, $2, $3, $4, $5, $6, $7, $8)"
+	tx, err := d.DB.BeginTx(*ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	_, err = tx.ExecContext(*ctx, stmt, Event.Id, Event.Location, Event.StartTime, Event.AvailableTickets, Event.TicketsTypes, Event.EventName, Event.TotalCapacity)
+	if err != nil {
+		return err
+	}
+	tx.Commit()
+	return nil
 }
